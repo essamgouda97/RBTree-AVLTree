cec54d32b627f222eb2248cf804797e20b4426e1
Samantha Barua Chowdhury <sc12@ualberta.ca>
2020-11-29 15:10:12 -0700

    Design Documentation

---
+++ project_rust/target/debug/deps/colored-0f1dd99115843573.d
@@ -0,0 +1,10 @@
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\colored-0f1dd99115843573.rmeta: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\color.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\control.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\style.rs
+
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\libcolored-0f1dd99115843573.rlib: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\color.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\control.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\style.rs
+
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\colored-0f1dd99115843573.d: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\color.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\control.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\style.rs
+
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\lib.rs:
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\color.rs:
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\control.rs:
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\colored-2.0.0\src\style.rs:
---
+++ project_rust/target/debug/deps/lazy_static-3058c6f12d44e685.d
@@ -0,0 +1,8 @@
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\lazy_static-3058c6f12d44e685.rmeta: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\liblazy_static-3058c6f12d44e685.rlib: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\saman\Desktop\ECE522-Project\project_rust\target\debug\deps\lazy_static-3058c6f12d44e685.d: C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs:
+C:\Users\saman\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs:
